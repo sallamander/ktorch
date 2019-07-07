451533f7947a48a14497109028ac1879e4e89bce
Sean Sall <sean@arterys.com>
2019-07-07 01:58:01 +0000

    Add categorical_accuracy metric and tests

---
+++ ktorch/metrics.py
@@ -0,0 +1,19 @@
+"""Metrics to use during training"""
+
+import torch
+
+
+def categorical_accuracy(y_true, y_pred):
+    """Return the accuracy in a multi-class problem
+
+    :param y_true: ground truth target values, of shape (batch_size, )
+    :type y_true: torch.Tensor
+    :param y_pred: predicted target values, of shape (batch_size, num_classes)
+    :type y_pred: torch.Tensor
+    :return: categorical accuracy
+    :rtype: numpy.ndarray
+    """
+
+    return torch.sum(
+        y_true == torch.argmax(y_pred, dim=-1)
+    ).numpy() / y_true.shape[0]
---
+++ tests/unit_tests/test_metrics.py
@@ -0,0 +1,38 @@
+"""Unit tests for metrics"""
+
+import numpy as np
+import torch
+
+from metrics import categorical_accuracy
+
+
+def test_categorical_accuracy():
+    """Tests for categorical_accuracy"""
+
+    test_cases = [
+        {'y_true': [0, 1, 2],
+         'y_pred': [[0.2, 0.1, 0.1],
+                    [0.1, 0.2, 0.1],
+                    [0.1, 0.1, 0.2]],
+         'expected_accuracy': 1.0},
+        {'y_true': [0, 1, 2],
+         'y_pred': [[0.1, 0.2, 0.1],
+                    [0.2, 0.1, 0.1],
+                    [0.1, 0.2, 0.1]],
+         'expected_accuracy': 0.0},
+        {'y_true': [0, 1, 2],
+         'y_pred': [[0.2, 0.1, 0.1],
+                    [0.1, 0.2, 0.1],
+                    [0.1, 0.2, 0.1]],
+         'expected_accuracy': 0.66666},
+    ]
+
+    for test_case in test_cases:
+        y_true = np.array(test_case['y_true'])
+        y_true = torch.from_numpy(y_true).long()
+
+        y_pred = np.array(test_case['y_pred'])
+        y_pred = torch.from_numpy(y_pred)
+        
+        accuracy = categorical_accuracy(y_true, y_pred)
+        assert np.isclose(accuracy, test_case['expected_accuracy'])
