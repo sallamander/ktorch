fd30df2fd869b769072590a99cebf999e0c262d7
Sean Sall <sean@arterys.com>
2019-08-14 00:51:28 +0000

    Add TopK accuracy metric

--- ktorch/metrics.py
+++ ktorch/metrics.py
@@ -26,7 +26,7 @@ def categorical_accuracy(y_true, y_pred):
     """Return the accuracy in a multi-class (3+ class) problem
 
     Note that because this simply takes the argmax of y_pred over the channel
-    dimension (rather than thresholding it), it's not necessary y_pred
+    dimension (rather than thresholding it), it's not necessary that y_pred
     represent probabilities.
 
     :param y_true: ground truth target values, of shape
@@ -44,3 +44,38 @@ def categorical_accuracy(y_true, y_pred):
     return torch.mean(
         (y_true == torch.argmax(y_pred, dim=1)).float()
     ).cpu().numpy()
+
+
+class TopKCategoricalAccuracy(object):
+    """Calculate the Top-K categorical accuracy"""
+
+    def __init__(self, k=5):
+        """Init
+
+        :param k: number of elements to consider when calculating categorical
+         accuracy
+        :type k: int
+        """
+
+        self.k = k
+        self.name = 'top_{}_categorical_accuracy'.format(k)
+
+    def __call__(self, y_true, y_pred):
+        """Calculate the categorical accuracy
+
+        :param y_true: ground truth classifications, of shape (batch_size, )
+        :type y_true: torch.Tensor
+        ;param y_pred: predicted classifications of shape
+         (batch_size, n_classes)
+        :type y_pred: torch.Tensor
+        :return: top-k categorical accuracy for the provided batch
+        :rtype: float
+        """
+
+        top_k_classifications = torch.topk(y_pred, self.k)[1]
+        n_correctly_classified = torch.sum(
+            torch.eq(top_k_classifications, y_true.view(-1, 1))
+        )
+        n_correctly_classified = n_correctly_classified.float()
+
+        return (n_correctly_classified / y_true.shape[0]).tolist()
--- tests/unit_tests/test_metrics.py
+++ tests/unit_tests/test_metrics.py
@@ -130,3 +130,148 @@ class TestCategoricalAccuracy(object):
             assert np.isclose(
                 accuracy, test_case['expected_accuracy'], atol=1e-4
             )
+
+
+class TestTopKCategoricalAccuracy(object):
+    """Tests for TopKCategoricalAccuracy"""
+
+    @pytest.fixture(scope='class')
+    def y_true(self):
+        """"y_true object fixture
+
+        Ground truth classifications will be one of 5 classes.
+
+        :return: array of ground truth classifications of shape (10, )
+        :rtype: torch.Tensor
+        """
+
+        y_true = np.array([1, 3, 4, 2, 0, 1, 3, 2, 0, 4])
+        y_true = torch.from_numpy(y_true)
+
+        return y_true
+
+    @pytest.fixture(scope='class')
+    def y_pred(self):
+        """y_pred object_fixture
+
+        Predicted classifications will be for five separate classes.
+
+        :return: array of ground truth classifications of shape (10, 5)
+        :rtype: torch.Tensor
+        """
+
+        y_pred = np.array([
+            [0.1, 0.2, 0.3, 0.4, 0.0],
+            [0.2, 0.3, 0.3, 0.075, 0.125],
+            [0.025, 0.05, 0.075, 0.15, 0.70],
+            [0.2, 0.2, 0.2, 0.3, 0.1],
+            [0.2, 0.3, 0.1, 0.25, 0.25],
+            [0.1, 0.2, 0.3, 0.4, 0.0],
+            [0.2, 0.3, 0.3, 0.075, 0.125],
+            [0.025, 0.05, 0.075, 0.15, 0.70],
+            [0.2, 0.2, 0.2, 0.3, 0.1],
+            [0.2, 0.3, 0.1, 0.25, 0.25],
+        ])
+        y_pred = torch.from_numpy(y_pred)
+
+        return y_pred
+
+    def test_init(self):
+        """Test __init__
+
+        This simply tests that all attributes are set correctly.
+        """
+
+        test_cases = [
+            {'k':  1, 'expected_name': 'top_1_categorical_accuracy'},
+            {'k':  2, 'expected_name': 'top_2_categorical_accuracy'},
+            {'k':  4, 'expected_name': 'top_4_categorical_accuracy'},
+            {'k':  5, 'expected_name': 'top_5_categorical_accuracy'}
+        ]
+
+        for test_case in test_cases:
+            k = test_case['k']
+            metric = TopKCategoricalAccuracy(k=k)
+
+            assert metric.k == k
+            assert metric.name == test_case['expected_name']
+
+    def test_call__k_of_1(self, y_true, y_pred):
+        """Test __call__ method when k=1
+
+        :param y_true: y_true object fixture
+        :type y_true: torch.Tensor
+        :param y_pred: y_predobject fixture
+        :type y_pred: torch.Tensor
+        """
+
+        mock_metric = MagicMock()
+        mock_metric.k = 1
+        mock_metric.__call__ = TopKCategoricalAccuracy.__call__
+
+        expected_categorical_accuracy = 0.10
+        categorical_accuracy = mock_metric.__call__(
+            self=mock_metric, y_true=y_true, y_pred=y_pred
+        )
+
+        assert np.isclose(categorical_accuracy, expected_categorical_accuracy)
+
+    def test_call__k_of_2(self, y_true, y_pred):
+        """Test __call__ method when k=2
+
+        :param y_true: y_true object fixture
+        :type y_true: torch.Tensor
+        :param y_pred: y_predobject fixture
+        :type y_pred: torch.Tensor
+        """
+
+        mock_metric = MagicMock()
+        mock_metric.k = 2
+        mock_metric.__call__ = TopKCategoricalAccuracy.__call__
+
+        expected_categorical_accuracy = 0.30
+        categorical_accuracy = mock_metric.__call__(
+            self=mock_metric, y_true=y_true, y_pred=y_pred
+        )
+
+        assert np.isclose(categorical_accuracy, expected_categorical_accuracy)
+
+    def test_call__k_of_4(self, y_true, y_pred):
+        """Test __call__ method when k=4
+
+        :param y_true: y_true object fixture
+        :type y_true: torch.Tensor
+        :param y_pred: y_predobject fixture
+        :type y_pred: torch.Tensor
+        """
+
+        mock_metric = MagicMock()
+        mock_metric.k = 4
+        mock_metric.__call__ = TopKCategoricalAccuracy.__call__
+
+        expected_categorical_accuracy = 0.80
+        categorical_accuracy = mock_metric.__call__(
+            self=mock_metric, y_true=y_true, y_pred=y_pred
+        )
+
+        assert np.isclose(categorical_accuracy, expected_categorical_accuracy)
+
+    def test_call__k_of_5(self, y_true, y_pred):
+        """Test __call__ method when k=5
+
+        :param y_true: y_true object fixture
+        :type y_true: torch.Tensor
+        :param y_pred: y_predobject fixture
+        :type y_pred: torch.Tensor
+        """
+
+        mock_metric = MagicMock()
+        mock_metric.k = 5
+        mock_metric.__call__ = TopKCategoricalAccuracy.__call__
+
+        expected_categorical_accuracy = 1.0
+        categorical_accuracy = mock_metric.__call__(
+            self=mock_metric, y_true=y_true, y_pred=y_pred
+        )
+
+        assert np.isclose(categorical_accuracy, expected_categorical_accuracy)
